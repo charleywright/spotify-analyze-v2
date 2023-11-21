e6d71077f59af8fe159ea83afac978a59e2fec27
Charley Wright <charleywright06@gmail.com>
2023-11-21 23:13:06 +0000

    pineapple: Add very basic threading and proxy loop

--- pineapple/src/main.rs
+++ pineapple/src/main.rs
@@ -1,27 +1,28 @@
 use std::{
     io,
     net::{TcpListener, TcpStream},
+    thread,
 };
 
 mod dh;
 mod proxy_session;
 use proxy_session::ProxySession;
 
-fn handle_client(downstream: TcpStream) -> std::io::Result<()> {
-    // Connect to real AP
-    let upstream = TcpStream::connect("ap.spotify.com:4070").expect("Failed to connect to Spotify AP");
-    // Create proxy session
-    let mut session = ProxySession::new(downstream, upstream);
-    session.start()
-}
-
 fn main() -> io::Result<()> {
     let host = "192.168.1.120:4070";
     let listener = TcpListener::bind(host).unwrap_or_else(|_| panic!("Failed to bind to {}", host));
     println!("Listening on {}", host);
 
-    for stream in listener.incoming() {
-        let _ = handle_client(stream?);
+    for downstream in listener.incoming().flatten() {
+        thread::spawn(move || {
+            let upstream = TcpStream::connect("ap.spotify.com:4070").expect("Failed to connect to Spotify AP");
+            let mut session = ProxySession::new(downstream, upstream);
+            if let Err(error) = session.start() {
+                println!("[E] Failed to start proxy session: {}", error);
+                return;
+            }
+            session.run();
+        });
     }
 
     Ok(())
